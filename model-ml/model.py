dcfa8f82c806d8687b7132012a61ef9c4e300800
afernandocisneros <fernando.cisnerosgaytan@gmail.com>
2020-12-21 20:59:07 -0600

    Empezando con el testing

--- model-ml/model.py
+++ model-ml/model.py
@@ -8,3 +8,15 @@ print("Probando la conexion con GitHub y Jenkins")
 print("GitHub-Jenkins-Docker")
 
 print("Actualizando direccion proporcionada por ngrok")
+
+
+
+def suma(a, b):
+    for n in (a,b):
+        if not isinstance(n, int) and not isinstance(n, float):
+            return TypeError
+    return a + b
+
+
+r = suma(2,2.2)
+print("La suma es {}".format(r))
---
+++ model-ml/test_suma.py
@@ -0,0 +1,6 @@
+from model import *
+
+def test_suma():
+    assert suma(3.3,2) == 5.3
+
+
